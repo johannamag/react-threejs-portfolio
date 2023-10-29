61d52362a0d4ecfa47520d5ad56614a9869d0956
Johanna Magdalena <69107842+johannamag@users.noreply.github.com>
2023-10-29 11:11:11 +0100

    setup 2

---
+++ src/components/Contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Section = styled.div`
+  height: 100vh;
+  background-color: #301934;
+`
+
+export default function Contact() {
+  return (
+    <Section>Contact</Section>
+  )
+}
---
+++ src/components/Works.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Section = styled.div`
+  height: 100vh;
+  background-color: #301934;
+`
+
+function Works() {
+  return (
+    <Section>Works</Section>
+  )
+}
+
+export default Works
