bdc3b0b262de0bc37f8421ef1077920341aa3700
Johanna Magdalena <69107842+johannamag@users.noreply.github.com>
2023-10-29 16:37:34 +0100

    works page

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,15 +1,86 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
+
+const data = [
+  "Web Development",
+  "Design",
+  "Illustration",
+  "Product Design",
+  "Social Media",
+];
 
 const Section = styled.div`
   height: 100vh;
   scroll-snap-align: center;
-`
+  display: flex;
+  justify-content: center;
+`;
+const Container = styled.div`
+  width: 1200px;
+  display: flex;
+  justify-content: space-between;
+`;
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+const List = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+const ListItem = styled.li`
+  font-size: 65px;
+  font-weight: bold;
+  cursor: pointer;
+  color: transparent;
+  -webkit-text-stroke: 1px pink;
+  position: relative;
+
+  &::after {
+    content: "${(props) => props.text}";
+    position: absolute;
+    top: 0;
+    left: 0;
+    color: pink;
+    width: 0;
+    overflow: hidden;
+    white-space: nowrap;
+  }
+
+  &:hover {
+    &::after {
+      animation: moveText 0.5s linear both;
+
+      @keyframes moveText {
+        to {
+          width: 100%
+        }
+      }
+    }
+  }
+`;
+const Right = styled.div`
+  flex: 1;
+`;
 
 function Works() {
   return (
-    <Section>Works</Section>
-  )
+    <Section>
+      <Container>
+        <Left>
+          <List>
+            {data.map((d) => (
+              <ListItem key={d} text={d}>{d}</ListItem>
+            ))}
+          </List>
+        </Left>
+        <Right></Right>
+      </Container>
+    </Section>
+  );
 }
 
-export default Works
+export default Works;
